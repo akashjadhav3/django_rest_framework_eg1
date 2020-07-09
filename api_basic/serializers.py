d6436d92c0a8ea4cff16f2c9903c80568f534680
root <root@localhost.localdomain>
2020-07-09 19:25:19 +0530

    change article_detail to DRF Response and status

--- api_basic/serializers.py
+++ api_basic/serializers.py
@@ -4,4 +4,5 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['id','title', 'author']
+        #fields = ['id','title', 'author','email']
+        fields = "__all__" 
