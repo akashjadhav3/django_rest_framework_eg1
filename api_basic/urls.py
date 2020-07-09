02c147887ccb4751826ea886a506c17e76c83e72
root <root@localhost.localdomain>
2020-07-09 21:42:34 +0530

    Class Based Viwe in articleDetail

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import article_list,article_detail,ArticleAPIView
+from .views import article_list,article_detail,ArticleAPIView,ArticleDetails
 
 urlpatterns =[
     # path('article/',article_list),
     path('article/',ArticleAPIView.as_view()),
-    path('detail/<int:pk>/',article_detail),
+    # path('detail/<int:pk>/',article_detail),
+    path('detail/<int:id>/',ArticleDetails.as_view()),
 ]
--- api_basic/views.py
+++ api_basic/views.py
@@ -24,7 +24,36 @@ class ArticleAPIView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)      
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+
+
+class ArticleDetails(APIView):
+
+    def get_object(self,id):
+        try:
+            return Article.objects.get(id=id)
+        except Article.DoesNotExist:
+            return HttpResponse(status=HTTP_404_NOT_FOUND)
+        
+    def get(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)       
+
+    def delete(self, request, id):
+        article = self.get_object(id)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 
 
 
