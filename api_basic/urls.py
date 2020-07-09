27b17efa3bc699a87eaa27ae44efa8b9df071c0b
root <root@localhost.localdomain>
2020-07-09 22:48:06 +0530

    Generic,mixin view using CRUD DRF

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -4,7 +4,7 @@ from .views import article_list,article_detail,ArticleAPIView,ArticleDetails,Gen
 urlpatterns =[
     # path('article/',article_list),
     path('article/',ArticleAPIView.as_view()),
-    path('generic/article/',GenericAPIView.as_view()),
     # path('detail/<int:pk>/',article_detail),
     path('detail/<int:id>/',ArticleDetails.as_view()),
+    path('generic/article/<int:id>/',GenericAPIView.as_view()),
 ]
--- api_basic/views.py
+++ api_basic/views.py
@@ -16,12 +16,28 @@ from rest_framework import mixins
 
 
 
-class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin):
+class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin,mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
 
-    def get(self, request):
-        return self.list(request)
+    lookup_field ='id'
+
+    def get(self, request, id = None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+    def put(self, request, id=None):
+        return self.update(request,id)
+
+    def delete(self, request, id):
+        return self.destroy(request, id)
+
+
 
 
 
